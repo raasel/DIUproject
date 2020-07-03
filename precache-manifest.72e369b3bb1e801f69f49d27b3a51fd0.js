dd10a71ef4ebb1a01e21c36bac39f89dc04671b6
raasel <47408774+raasel@users.noreply.github.com>
2020-07-04 01:24:09 +0600

    Add files via upload

---
+++ precache-manifest.72e369b3bb1e801f69f49d27b3a51fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc0a4138c2de340fea2ed9205318f34",
+    "url": "/DIUproject/index.html"
+  },
+  {
+    "revision": "62a6b0c7b4da01a921ce",
+    "url": "/DIUproject/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "42e0ba50f6d860b6429f",
+    "url": "/DIUproject/static/css/main.5d1dd0a9.chunk.css"
+  },
+  {
+    "revision": "62a6b0c7b4da01a921ce",
+    "url": "/DIUproject/static/js/2.d8f4b5bc.chunk.js"
+  },
+  {
+    "revision": "42e0ba50f6d860b6429f",
+    "url": "/DIUproject/static/js/main.a9fe5dcb.chunk.js"
+  },
+  {
+    "revision": "1f7ffd3288bf57ae244b",
+    "url": "/DIUproject/static/js/runtime-main.439aac17.js"
+  }
+]);
